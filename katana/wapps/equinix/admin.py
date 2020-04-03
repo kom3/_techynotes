29d36dd5bb4027101f7fe5b38a761222fb108f9b
apathapa-warrior <anil.pathapati@us.fujitsu.com>
2020-04-03 15:46:06 +0530

    inital commit

--- katana/wapps/equinix/admin.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from django.contrib import admin
-from .models import equinixgroups
-# Register your models here.
-# admin.site.register(equinixgroups)
--- katana/wapps/equinix/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms  
-from katana.wapps.equinix.models import equinixgroups  
-class equinixgroupsForm(forms.ModelForm):  
-    class Meta:  
-        model = equinixgroups
-        fields = "__all__"  
-       
